d95002bb3af76341cb45f233f8476fea70d1f991
Richards-d <dienepapa90@gmail.com>
2022-06-02 17:13:33 -0700

    My commit 2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-#include <string.h>
-
-int main(void)  {
-
-/* initializing string */
-char *str = "Programming is like building a multilingual puzzle";
-
-/* writing to stdout */
-puts(str1);
-        
-return (0);
-
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)  
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
