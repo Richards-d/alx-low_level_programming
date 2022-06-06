4621dadcca24a8ee2564e84696c5ca338cce2bd5
Richards-d <dienepapa90@gmail.com>
2022-06-05 17:45:31 -0700

    My commit 2

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,8 +11,9 @@ int main(void)
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
-		putchar("%c", i);
+		putchar(i);
 	for (i = 'A'; i <= 'Z'; i++)
-		putchar("%c", i);
+		putchar(i);
+	putchar('\n');
 	return (0);
 }
