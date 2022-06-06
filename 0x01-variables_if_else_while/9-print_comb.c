c178e868c53e48dbe20b9e41da23b8a563aca8a2
Richards-d <dienepapa90@gmail.com>
2022-06-06 07:02:02 -0700

    My commit 2

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = '1'; i <= '3'; i++)
+		putchar(i);
+	for (j = '1'; j <= '3'; j++)
+		puthar(j);
+	for (k = '1'; k <= '3'; k++)
+		putchar(k);
+	putchar('\n');
+	return (0);
+}
