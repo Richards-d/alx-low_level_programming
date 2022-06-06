c3648364dbee312e486fb034cd12d139a003efd2
Richards-d <dienepapa90@gmail.com>
2022-06-06 07:36:35 -0700

    My commit 2

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = "1, "; i <= "3, "; i++)
+	for (i = '1'; i <= '3'; i++)
 		putchar(i);
-	for (j = "4, "; j <= "6, "; j++)
+	for (j = '4'; j <= '6'; j++)
 		putchar(j);
-	for (k = "7, "; k <= "9, "; k++)
+	for (k = '7'; k <= '9'; k++)
 		putchar(k);
 	putchar('\n');
 	return (0);
