ae2d61710d7c188cf6d7b63f6a6a7023eef49043
Richards-d <dienepapa90@gmail.com>
2022-06-08 08:56:09 -0700

    My commit 2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
-#include "main.h"
 /**
  * main - prints _putchar, followed by a new line.
  *
- * Return: Always 0 (success)
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
