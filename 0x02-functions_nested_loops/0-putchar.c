f7caa7a32b0c9b2136a534d16b7a9fc3ca7f79db
Richards-d <dienepapa90@gmail.com>
2022-06-08 09:51:36 -0700

    My commit 2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,11 @@
 #ifdef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-int _putchar(char c);
-#endif /* MAIN_H */
 /**
  * main - prints _putchar, followed by a new line.
  *
  * Return: Always 0 (success)
  */
+#endif /* MAIN_H */
 int _putchar(char c);
 {
 	_putchar('_');
