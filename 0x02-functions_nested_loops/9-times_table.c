2a4cc993d5d057231fcc042bf34fd80af50edd26
Richards-d <dienepapa90@gmail.com>
2022-06-10 09:45:01 -0700

    My commit 2

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,8 +7,7 @@ void times_table(void)
 	int i, j, k;
 
 	for (i = 0; i < 10; i++);
-	{
-		for (j = 0; j < 10; j++);
+	for (j = 0; j < 10; j++);
 		{
 			k = j * i;
 			if (j == 0)
@@ -31,6 +30,5 @@ void times_table(void)
 				_putchar((k % 10) + '0');
 			}
 		}
-	}
 	_putchar('\n');
 }
