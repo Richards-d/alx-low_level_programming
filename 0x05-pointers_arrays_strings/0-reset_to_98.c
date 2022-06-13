c8315b29a358056256fdb9d51dd0e5e19fab126a
Richards-d <dienepapa90@gmail.com>
2022-06-13 08:54:33 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - takes a pointer to an int as parameter and updates the value it
+ * points to to 98.
+ */
+void reset_to_98(int *n)
+{
+	int var = 98;
+	int *n;
+
+	_putchar("Value of *n variable: %d\n, *n");
+	return (0);
+}
