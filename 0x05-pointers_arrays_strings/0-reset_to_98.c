16e79e6088f683bf7a90209f45b676f2e1ff3fde
Richards-d <dienepapa90@gmail.com>
2022-06-15 05:20:20 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,11 @@
 #include "main.h"
 /**
- * main - takes a pointer to an int as parameter and updates the value it
+ * reset_to_98 - takes a pointer to an int as parameter and updates the value it
  * points to to 98.
+ * @n: integer pointer to a variable to be updates
+ * Return: nothing
  */
 void reset_to_98(int *n)
 {
-	int n = 98;
-	int *ip;
-
-	_putchar("Value of *ip variable: %d\n", *ip);
-	return (0);
+	*n = 98;
 }
